b15cc5cda8aaf5bb5d2e7430355b97c24ef0fb30
Dima Parfenov <lebwa82@gmail.com>
2021-11-14 01:52:38 +0300

    simple handshake done

--- server/get_client_ips.py
+++ server/get_client_ips.py
@@ -10,18 +10,28 @@ def connect(addr,search_port):
         print(f"Found client on {addr}")
         global clients
         clients.append(addr)
+        sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((str(addr),int(search_port)))
+        print("connect done")
+        data = sock.recv(1024)
+        print(data.decode('utf8'))
+        sock.send('Hi, glad to see u'.encode('utf8'))
+        if str(data.decode('utf8')) == message:
+            print("he is a good boy!")
+        sock.close()
 
-
+    
 
 clients = []
-
+message = "Hi, glad to see u"
 
 def get_client_ips( target_interface="enp7s0", family='AF_INET', search_port = "51815" , debug = True):
     '''This function scans the network and looks for open ports in it. 
     Returns a list of ip addresses that have the corresponding port open  '''
 
-    if debug==True:
-        search_port=80
+    #if debug==True:
+    #    search_port=80
 
     interface_list=netifaces.interfaces() #получить список интерфейсов
     if debug == True:
@@ -46,7 +56,8 @@ def get_client_ips( target_interface="enp7s0", family='AF_INET', search_port = "
     s=ipv4+"/"+mask
     net = ipaddress.IPv4Network(s, strict=False)
     if debug == True:
-        print("my net is", net)  
+        print("my net is", net)
+        print("search_port is",search_port)  
     
     for addr in net:
         t=Thread(target = connect, args = (addr,search_port))
@@ -54,8 +65,13 @@ def get_client_ips( target_interface="enp7s0", family='AF_INET', search_port = "
     return clients
 
 
+    
+
+
+
 if __name__=='__main__':
     start_time=time.time()
     clients=get_client_ips("enp7s0",'AF_INET')
     print(clients)
     print (f"program time is  {time.time() - start_time} seconds")
+
