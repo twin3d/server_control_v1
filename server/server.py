be4f4d23c9ac798f6aa62a5949b332f0b20cdce2
Mikhail Solovyanov <miksolo@yandex.ru>
2021-12-06 20:59:13 +0300

    Mikhail Пн 06 дек 2021 20:59:13 MSK

--- server/server.py
+++ server/server.py
@@ -40,10 +40,10 @@ def connect(addr,search_port, socket_timeout):
 
 
 def get_client_ips( target_interface="enp3s0", family='AF_INET',
- search_port = "51815", mask = None, config_file = "./server/config.ini", output_type = "output_file"):
+ search_port = "51815", mask = None, config_file = "./server/config.ini", output_file = None):
     '''This function scans the network and looks for open ports in it. 
     Returns a list of ip addresses that have the corresponding port open  '''
-
+    import sys,logging
     global clients
     clients = []
     global clients_dict
@@ -51,16 +51,28 @@ def get_client_ips( target_interface="enp3s0", family='AF_INET',
 
     
     #get values from config
-    config = configparser.ConfigParser()
-    if not os.path.isfile(config_file):
-        logging.error("No config file")
-        return "NO_CONFIG_FILE"    
-    config.read(config_file)   
-    search_port = config.get("GENERAL","Port")
-    socket_timeout = config["GENERAL"]["Timeout"]
-    logging.debug(f"search_port is {search_port}, socket_timeout is  {socket_timeout}")
-
-
+    try:
+        config = configparser.ConfigParser()  
+        config.read(config_file)   
+        search_port = config.get("GENERAL","Port")
+        try:
+            log_level = config.get("GENERAL","log_level")
+        except Exception:
+            log_level = "debug"
+        socket_timeout = config["GENERAL"]["Timeout"]
+        
+        logger = logging.getLogger()
+        if log_level == "debug":
+            logger.setLevel(logging.DEBUG)
+        elif log_level == "warning":
+            logger.setLevel(logging.ERROR)
+        logger.debug(f"Debug level is {log_level}")
+        
+        logging.debug(f"search_port is {search_port}, socket_timeout is  {socket_timeout}")
+    except Exception as err_msg:
+        logging.error(f"!!! Error !!! when reading config file! Check {config_file} file, or get default one. More exact: {err_msg}.")
+        print(f"!!! Error !!! when reading config file! Check {config_file} file, or get default one. More exact: {err_msg}.")
+        sys.exit()
 
     interface_list=netifaces.interfaces() #получить список интерфейсов
     logging.debug(f"interface_list is {interface_list}")
@@ -97,12 +109,15 @@ def get_client_ips( target_interface="enp3s0", family='AF_INET',
         thread.join()
 
     #return clients
-    output_print(output_type)
+    if output_file != None:
+        print_to_file(output_file)
+    # ВСЕГДА RETURN!!!
 
 
-def output_print(output_type = "python"):
-    logging.debug(f"output_type is {output_type}")
-    if output_type == "python":
+def output(filename):
+    '''Выводит вывод в файл, если он передан.'''
+    logging.debug(f"output_type is {filename}")
+    if filename == None:
         output_dict = {}
         for client in clients:
             host = str(client)
@@ -110,16 +125,19 @@ def output_print(output_type = "python"):
 
         return output_dict
 
-    elif output_type == "output_file":
+    elif filename == "output_file":
         return print_to_file()
-    else:
-        logging.error("You enter wrong output_type")
-        return "ERROR_OUTPUT"
+    #else:
+    #    logging.error("You entered wrong output_type")
+    #    return "ERROR_OUTPUT"
 
 
 
-def print_to_file():
-    file = open("output_file.txt", "w")
+def print_to_file(filename):
+    try:
+        file = open(filename, "w")
+    except Exception as err_msg:
+        logging.error("You entered wrong filename. Error is {err_msg}")
     logging.debug("writing to file")
     #write big section
     file.write("Big\n")
