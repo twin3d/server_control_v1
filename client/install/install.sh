8bea6e018b2d065a6444ddc76d968bcc6a2b2021
Mikhail Solovyanov <miksolo@yandex.ru>
2021-12-07 15:18:39 +0300

    install script

---
+++ client/install/install.sh
@@ -0,0 +1,26 @@
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+
+# Installing python packages
+
+if [ $(pip3 --version) -ne 0 ]; then
+    sudo apt install python3-pip
+fi
+
+pip3 install -r $DIR/requirements.txt
+
+client_target_dir=$(realpath $DIR/..)
+
+#Create config
+
+# Create unit file
+USER_SYSTEMD_DIR=/home/$USER/.config/systemd/user
+mkdir -p $USER_SYSTEMD_DIR
+
+cp "scanner_watchdog.service" "scanner_watchdog.service_temp"
+
+sed -i "s#%CLIENT_PATH#$client_target_dir#g" "scanner_watchdog.service_temp"
+
+cp "scanner_watchdog.service_temp" "$USER_SYSTEMD_DIR/scanner_watchdog.service"
+
