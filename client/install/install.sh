da0a8151e80e25b25424c97a39617984300cd06d
Mikhail Solovyanov <miksolo@yandex.ru>
2021-12-21 19:29:52 +0300

    sudo install

--- client/install/install.sh
+++ client/install/install.sh
@@ -6,16 +6,18 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+#USER_SYSTEMD_DIR=/home/$USER/.config/systemd/user
+USER_SYSTEMD_DIR=/etc/systemd/system
 
 # Installing python packages
-echo -e "Checking python fieles"
+echo -e "Checking python fiels"
 
 if [ "$(pip3 --version)" -ne 0 ]; then
     sudo apt update
     sudo apt install python3-pip
 fi
 
-pip3 install -r $DIR/requirements.txt
+sudo pip3 install -r $DIR/requirements.txt
 
 client_target_dir=$(realpath $DIR/..)
 
@@ -26,23 +28,20 @@ cp "$client_target_dir/config.ini" "$client_target_dir/configuration/configurati
 # Create unit file
 echo -e "Creating systemctl unit from 'scanner_watchdog.service_temp'"
 
-USER_SYSTEMD_DIR=/home/$USER/.config/systemd/user
-mkdir -p $USER_SYSTEMD_DIR
-
+#mkdir -p $USER_SYSTEMD_DIR
 cp "scanner_watchdog.service" "scanner_watchdog.service_temp"
 
 sed -i "s#%CLIENT_PATH#$client_target_dir#g" "scanner_watchdog.service_temp"
 sed -i "s#%USER#$USER#g" "scanner_watchdog.service_temp"
 
-cp "scanner_watchdog.service_temp" "$USER_SYSTEMD_DIR/scanner_watchdog.service"
+sudo cp "scanner_watchdog.service_temp" "$USER_SYSTEMD_DIR/scanner_watchdog.service"
 
 [[ $? -eq 0 ]] && { echo -e "Systemctl unit copied"; }
 
 rm "scanner_watchdog.service_temp"
 
 #Systemctl activation
-systemctl --user daemon-reload && systemctl --user restart scanner_watchdog.service
-systemctl --user restart scanner_watchdog.service
+sudo systemctl  daemon-reload && systemctl restart scanner_watchdog.service
 [[ $? -eq 0 ]] && { echo -e "Systemctl started"; }
-systemctl --user enable scanner_watchdog.service
+sudo systemctl  enable scanner_watchdog.service
 [[ $? -eq 0 ]] && { echo -e "Systemctl enabled"; }
